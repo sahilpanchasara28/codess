47b6a571beed614240b8c458483d334120943dad
Sahil Panchasara <63531633+sahilproo@users.noreply.github.com>
2022-05-20 17:22:27 +0530

    Time: 61 ms (39.11%), Space: 8 MB (10.48%) - LeetHub

---
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    
+    bool isBal(unordered_map<char, int> &m, int n)
+    {
+        if(m['Q'] <= n && m['W'] <= n && m['E'] <= n && m['R'] <= n)
+            return true;
+        return false;
+    }
+    
+    int balancedString(string s) {
+        unordered_map<char, int> m;
+        int n = s.size();
+        
+        for(int i=0; i<n; i++)
+            m[s[i]]++;
+        
+        if(isBal(m, n/4))
+            return 0;
+        
+        int left = 0, right, ans = INT_MAX;
+        
+        for(right = 0; right < n; right++)
+        {
+            m[s[right]]--;
+            
+            if(isBal(m, n/4))
+            {
+                while(left <= right && isBal(m, n/4))
+                {
+                    ans = min(ans, right-left+1);
+                    m[s[left]]++;
+                    left++;
+                }
+            }
+        }
+        
+        return ans;
+        
+    }
+};
