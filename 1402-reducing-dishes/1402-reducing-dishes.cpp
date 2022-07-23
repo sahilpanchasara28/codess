9e45a095bd76d87c6faca54adb9332cceba10e99
Sahil Panchasara <63531633+sahilpanchasara28@users.noreply.github.com>
2022-07-23 17:23:04 +0530

    Time: 44 ms (25.58%), Space: 21 MB (10.92%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -32,7 +32,7 @@ public:
         
         for(int ind=n-1; ind>=0; ind--)
         {
-            for(int time=n-1; time>=0; time--)
+            for(int time=ind; time>=0; time--)
             {
                 int include = sat[ind]*(time+1) + dp[ind+1][time+1];
                 int exclude = dp[ind+1][time];
