ca5f2d7ce9fd938cdb36b08391df1e42e6df5f8c
Sahil Panchasara <63531633+sahilpanchasara28@users.noreply.github.com>
2022-07-28 12:37:48 +0530

    Time: 19 ms (36.65%), Space: 7.3 MB (45.59%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,8 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s==t;
+        if(s.size() != t.size())
+            return false;
+        
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+        // return s==t;
+        
+        unordered_map<char, int> m1, m2;
+        for(char i: s)
+            m1[i]++;
+        for(char i: t)
+            m2[i]++;
+        
+        for(auto i: s)
+            if(m1[i] != m2[i])
+                return false;
+        return true;
     }
 };
