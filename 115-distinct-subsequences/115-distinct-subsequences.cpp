8a712303e35a6e1fe8add304de995fa79a5f82f9
Sahil Panchasara <63531633+sahilpanchasara28@users.noreply.github.com>
2022-08-13 18:43:56 +0530

    Time: 41 ms (79.11%), Space: 6.4 MB (96.41%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -126,9 +126,9 @@ int spaceMore(string &t, string &s, int n, int m)
     // vector<vector<int>> dp(lt, vector<int>(ls, -1));
     // return recursiveMem(t, s, lt-1, ls-1, dp);
     
-    return tabulation(t, s, lt, ls);
+    // return tabulation(t, s, lt, ls);
     
-//     return space(t, s, lt, ls);
+    return space(t, s, lt, ls);
     
     // return spaceMore(t, s, lt, ls);
     }
