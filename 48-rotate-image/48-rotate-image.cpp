0b64a261cf7d9c05608264afb17d210d512becc8
Sahil Panchasara <63531633+sahilpanchasara28@users.noreply.github.com>
2022-08-30 19:58:15 +0530

    Time: 8 ms (9.58%), Space: 7.1 MB (73.79%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& mat) {
+        int n = mat.size();
+        for(int i=0; i<n-1; i++)
+            for(int j=i+1; j<n; j++)
+                swap(mat[i][j], mat[j][i]);
+        
+        for(int i=0; i<n; i++)
+            reverse(mat[i].begin(), mat[i].end());
+    }
+};
