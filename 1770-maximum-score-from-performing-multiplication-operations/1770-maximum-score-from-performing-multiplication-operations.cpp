20f0bcbebfe4559bc6108fa466be0f78a20096b9
Sahil Panchasara <63531633+sahilpanchasara28@users.noreply.github.com>
2022-09-16 18:00:14 +0530

    Time: 500 ms (81.31%), Space: 50.6 MB (99.60%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -32,13 +32,34 @@ public:
         return dp[0][0];
     }
     
+    int solveTabSpace(vector<int> &a, vector<int> &b)
+    {
+        int n = a.size(), m = b.size();
+        vector<int> curr(m+1, 0);
+        vector<int> prev(m+1, 0);
+
+        for(int ind=m-1; ind>=0; ind--)
+        {
+            for(int s=ind; s>=0; s--)
+            {
+                int op1 = b[ind]*a[s] + prev[s+1];
+                int op2 = b[ind]*a[n-1-ind+s] + prev[s];
+                curr[s] = max(op1, op2);
+            }
+            prev = curr;
+        }
+        
+        return prev[0];
+    }
+    
     int maximumScore(vector<int>& a, vector<int>& b) {
         
         // int n = a.size(), m = b.size();
         // vector<vector<int>> dp(m+1, vector<int> (m+1, INT_MIN));
         // return solve(a, b, 0, 0, n, m, dp);
         
-        return solveTab(a, b);
+        // return solveTab(a, b);
         
+        return solveTabSpace(a, b);
     }
 };
