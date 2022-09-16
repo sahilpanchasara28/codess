da97bae9359cdc67e06541fc59fc7fff05977d58
Sahil Panchasara <63531633+sahilpanchasara28@users.noreply.github.com>
2022-09-16 17:58:40 +0530

    Time: 659 ms (57.41%), Space: 119.8 MB (72.63%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -14,10 +14,31 @@ public:
         return dp[ind][s] = max(op1, op2);
     }
     
+    int solveTab(vector<int> &a, vector<int> &b)
+    {
+        int n = a.size(), m = b.size();
+        vector<vector<int>> dp(m+1, vector<int> (m+1, 0));
+
+        for(int ind=m-1; ind>=0; ind--)
+        {
+            for(int s=ind; s>=0; s--)
+            {
+                int op1 = b[ind]*a[s] + dp[ind+1][s+1];
+                int op2 = b[ind]*a[n-1-ind+s] + dp[ind+1][s];
+                dp[ind][s] = max(op1, op2);
+            }
+        }
+        
+        return dp[0][0];
+    }
+    
     int maximumScore(vector<int>& a, vector<int>& b) {
         
-        int n = a.size(), m = b.size();
-        vector<vector<int>> dp(m+1, vector<int> (m+1, INT_MIN));
-        return solve(a, b, 0, 0, n, m, dp);
+        // int n = a.size(), m = b.size();
+        // vector<vector<int>> dp(m+1, vector<int> (m+1, INT_MIN));
+        // return solve(a, b, 0, 0, n, m, dp);
+        
+        return solveTab(a, b);
+        
     }
 };
