2e53e613085d151b6a266b09a018732153e62922
Sahil Panchasara <63531633+sahilpanchasara28@users.noreply.github.com>
2022-11-10 18:28:36 +0530

    Added solution - LeetHub

---
+++ Minimum XOR value pair - GFG/minimum-xor-value-pair.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+
+class Solution{   
+public:
+    int minxorpair(int N, int arr[]){    
+        sort(arr, arr+N);
+        
+        int ans = 1e9;
+        for(int i=1; i<N; i++)
+            ans = min(ans, arr[i-1]^arr[i]);
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N, X;
+        cin >> N;
+        int arr[N];
+        for(int i = 0; i < N; i++){
+            cin >> arr[i];
+        }
+
+        Solution ob;
+        cout << ob.minxorpair(N, arr) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
