7c63501452ccb27891475d55889ee74ab553e52f
Sahil Panchasara <63531633+sahilpanchasara28@users.noreply.github.com>
2022-11-14 17:29:55 +0530

    Added solution - LeetHub

--- Print Anagrams Together - GFG/print-anagrams-together.cpp
+++ Print Anagrams Together - GFG/print-anagrams-together.cpp
@@ -8,24 +8,105 @@ using namespace std;
 // } Driver Code Ends
 //User function Template for C++
 
+class LinkedList {
+    public:
+        int index;
+        LinkedList* next;
+    
+    LinkedList(int ind) {
+        index = ind;
+        next = NULL;
+    }
+};
+
+class TrieNode {
+    public:
+        TrieNode* children[26];
+        bool isTerminal;
+        LinkedList* head;
+    
+    TrieNode() {
+        for(int i=0; i<26; i++)
+            children[i] = NULL;
+        isTerminal = false;
+        head = NULL;
+    }
+};
+
 class Solution{
   public:
-    vector<vector<string> > Anagrams(vector<string>& string_list) {
-        
-    unordered_map<string, vector<string>> m;
-    vector<vector<string>> ans;
-    
-    for(auto st: string_list)
-    {
-        string temp = st;
-        sort(temp.begin(), temp.end());
-        m[temp].push_back(st);
+  TrieNode* root;
+  
+  Solution() {
+        root = new TrieNode();
     }
     
-    for(auto i: m)
-        ans.push_back(i.second);
-    
-    return ans;
+    void insert(string word, int ind) {
+        TrieNode* node = root;
+        
+        for(int i=0; i<word.length(); i++)
+        {
+            char ch = word[i];
+            if(node->children[ch-'a'] == NULL)
+                node->children[ch-'a'] = new TrieNode();
+            
+            node = node->children[ch-'a'];
+        }
+        
+        if(node->isTerminal) //word already present
+        {
+            LinkedList* temp = node->head;
+            while(temp->next != NULL)
+                temp = temp->next;
+                
+            temp->next = new LinkedList(ind);
+        }
+        else
+        {
+            node->isTerminal = true;
+            node->head = new LinkedList(ind);
+        }
+     }
+ 
+    vector<string> search(string word, vector<string> &st) {
+        TrieNode* node = root;
+        
+        for(int i=0; i<word.length(); i++)
+        {
+            char ch = word[i];
+            node = node->children[ch-'a'];
+        }
+        
+        vector<string> ans;
+        
+        LinkedList* temp = node->head;
+        while(temp != NULL)
+        {
+            ans.push_back(st[temp->index]);
+            temp = temp->next;
+        }
+
+        return ans;
+    }
+  
+    vector<vector<string> > Anagrams(vector<string>& st) {
+        
+        unordered_set<string> list;
+        for(int i=0; i<st.size(); i++)
+        {
+            string stCopy = st[i];
+            sort(stCopy.begin(), stCopy.end());
+            list.insert(stCopy);
+            
+            insert(stCopy, i);
+        }
+        
+        vector<vector<string>> ans;
+        
+        for(auto i: list)
+            ans.push_back(search(i, st));
+        
+        return ans;
     }
 };
 
