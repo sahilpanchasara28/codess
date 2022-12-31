450d5e5f9222f87ad3bc7343165c9e316b31de76
Sahil Panchasara <63531633+sahilpanchasara28@users.noreply.github.com>
2022-12-31 19:10:28 +0530

    Time: 3 ms (86.88%), Space: 7.7 MB (69.91%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -25,8 +25,29 @@ public:
         
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> dp(n, vector<int> (m, -1));
         
-        return dfs(0, 0, n, m, grid, dp);
+        if (grid[0][0] == 1 or grid[n-1][m-1] == 1) 
+            return 0; 
+        
+        vector<vector<int>> dp(n, vector<int> (m, 0));
+                
+        dp[0][0] = 1;
+        
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if((i == 0 and j == 0) or grid[i][j] == 1)
+                    continue;
+                else
+                {
+                    int down = (i > 0) ? dp[i-1][j] : 0;
+                    int right = (j > 0) ? dp[i][j-1] : 0;
+                    dp[i][j] = down+right;
+                }
+            }
+        }
+        
+        return dp[n-1][m-1];
     }
 };
